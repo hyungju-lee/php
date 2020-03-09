0b36a63dea620e1b09dc5e1483ba5d88ec00256f
hyungju-lee <beegizee1220@gmail.com>
2020-03-10 01:29:56 +0900

    mysql study

---
+++ php/108-1-createDB.php
@@ -0,0 +1,41 @@
+<?php
+    /*
+     * 데이터베이스 생성하기
+     * 학습 내용 : 데이터베이스를 생성하는 방법에 대해 학습합니다.
+     * 힌트 내용 : 데이터베이스를 생성 명령문은 CREATE DATABASE 입니다.
+     *
+     * MySQL에서 사용할 데이터베이스를 생성하겠습니다.
+     * 보통 1개의 서비스에 1개의 데이터베이스를 사용합니다.
+     *
+     * CREATE DATABASE 데이터베이스명
+     *
+     * 위와 같은 데이터베이스에서 사용하는 명령문을 쿼리문이라고 부릅니다.
+     * 생성할 데이터베이스의 이름은 [php200Example]로 하겠습니다.
+     * 쿼리문을 실행하면 mysqli 클래스 내에 있는 query 메소드를 사용합니다.
+     * query 메소드의 아규먼트로 쿼리문을 입력합니다.
+     *
+     * query("쿼리문");
+     *
+     *
+     * */
+
+    $host = "localhost";
+    $user = "root";
+    $pw = "";
+    $dbConnect = new mysqli($host, $user, $pw);
+    $dbConnect->set_charset("utf8");
+
+    if (mysqli_connect_errno()) {
+        echo '데이터베이스 접속 실패';
+    } else {
+        $sql = "CREATE DATABASE php200Example2";
+        $res = $dbConnect->query($sql);
+
+        if ($res) {
+            echo "데이터베이스 생성 완료";
+        } else {
+            echo "데이터베이스 생성 실패";
+        }
+    }
+?>
+
---
+++ php/110-showTables.php
@@ -0,0 +1,15 @@
+<?php
+  echo "int 또는 integer : 숫자 - 정수";
+  echo "<br>";
+  echo "double : 숫자 - 소수";
+  echo "<br>";
+  echo "string : 문자열";
+  echo "<br>";
+  echo "boolean : 논리값(값 : true, false)";
+  echo "<br>";
+  echo "NULL : 없는값(값 : null)";
+  echo "<br>";
+  echo "array : 배열";
+  echo "<br><br>";
+  echo "NULL 값은 없는 값을 의미하는 데이터형입니다. <br>
+  값이 없다는 것 또한 변수의 �
